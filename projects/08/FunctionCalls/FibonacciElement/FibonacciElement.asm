a145d6e50bd3990175ce2ea8936722efdcba86ae
Abhay Nayar <abhaynayar@gmail.com>
2020-09-09 20:18:06 +0530

    finished week 10

--- projects/08/FunctionCalls/FibonacciElement/FibonacciElement.asm
+++ projects/08/FunctionCalls/FibonacciElement/FibonacciElement.asm
@@ -411,6 +411,65 @@ A=M
 0;JMP
 
 
+// ASM_FILE: FibonacciElement.asm
+// bootstrap code
+@256
+D=A
+@SP
+M=D
+// call Sys.init 0
+@LABEL0
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@LCL
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@ARG
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@THIS
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@THAT
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+D=M
+@0
+D=D-A
+@5
+D=D-A
+@ARG
+M=D
+@SP
+D=M
+@LCL
+M=D
+@Sys.init
+0;JMP
+(LABEL0)
+
+
 // ASM_FILE: Sys.asm
 // function Sys.init 0
 (Sys.init)
